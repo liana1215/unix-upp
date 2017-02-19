8727f9cfb717b33ab0ebfbba51936931b22bd631
tasuku <tasuku@gmail.com>
2017-02-19 14:42:21 +0900

    updated

--- src/projects/wow/wow.c
+++ src/projects/wow/wow.c
@@ -5,6 +5,7 @@
 #include    <fcntl.h>
 #include    <stdlib.h>
 #include    <assert.h>
+#include    <errno.h>
 
 #include    "wtmplib.h" 
 #include    "wtmpsearch.h"
@@ -15,9 +16,26 @@
  */
 #define USAGE "usage: [-f] input_file year month day [-s|-b]\n"
 
+
+long int get_arg(char **argv) {
+    char *endptr = NULL;
+    char *str = NULL;
+    long int ret = 0;
+    
+    errno = 0;    
+    str = argv[optind++];
+    ret = strtol(str, &endptr, 10);
+
+    if (endptr == str) {
+        fprintf(stderr, "No digits were found.\n");
+        exit(1);
+    }
+    return ret;
+}
+
 int main(int argc, char **argv)
 {
-    char* f_name = UTMP_FILE;
+    char* f_name = WTMP_FILE;
     int opt = 0; 
     int year = 0;
     int month = 0;
@@ -47,24 +65,20 @@ int main(int argc, char **argv)
     if (optind == argc) {optind = 1;} 
 
     assert(argc > 3 && "usage: [-f] input_file year month day [-s|-b]\n");
-    year = atoi(argv[optind++]) - 1900;
-    month = atoi(argv[optind++]) - 1;
-    day = atoi(argv[optind++]);     
+    
+    year = get_arg(argv) - 1900;
+    month = get_arg(argv) - 1;
+    day = get_arg(argv);
+
+    if (utmp_open(f_name) == -1 ) {
+        fprintf(stderr,"%s: cannot open %s\n", *argv, f_name);
+        exit(1);
+    }
 
     if (!seq_bin) {
-        if (utmp_open(f_name) == -1 ) {
-            fprintf(stderr,"%s: cannot open %s\n", *argv, f_name);
-            exit(1);
-        }
-
         int fsize = utmp_fsize(f_name);
         fetch_bsearch(year, month, day, fsize);
-
     } else {
-        if (utmp_open(f_name) == -1 ) {
-            fprintf(stderr,"%s: cannot open %s\n", *argv, f_name);
-            exit(1);
-        }
         fetch_sequential(year, month, day);
     }
     utmp_close();
