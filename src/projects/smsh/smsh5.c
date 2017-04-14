e2e0f35af2796ea545524c0c50807d223b5d5d74
surfertas <tasuku@gmail.com>
2017-04-15 00:14:57 +0900

    add var replace

--- src/projects/smsh/smsh5.c
+++ src/projects/smsh/smsh5.c
@@ -1,10 +1,13 @@
+#include    <ctype.h>
 #include	<stdio.h>
 #include	<stdlib.h>
 #include	<unistd.h>
 #include	<signal.h>
 #include	<sys/wait.h>
+#include    "flexstr.h"
 #include	"smsh.h"
 #include	"splitline.h"
+#include    "string.h"
 #include	"varlib.h"
 #include	"process.h"
 
@@ -18,6 +21,53 @@
 #define	DFL_PROMPT	"> "
 
 void	setup();
+
+
+char* is_in_str(char *str)
+/*
+ * purpose: determines if a string is a legal variable name
+ * returns: 0 for no, 1 for yes
+ */
+{
+    FLEXSTR fname;                      /* Placeholder for new string */
+    fs_init(&fname, 0);
+
+    while (1) { 
+        char tmp[2048];                 /* Placeholder for string after $ */
+        char *cp;
+
+        if ((cp = strchr(str, '$')) == NULL) {
+            break;
+        }
+        
+        int i = 0;
+        while (*++cp != '\0' && *cp != ' ' && *cp != '.') {
+            tmp[i] = *cp;
+            i++;
+        }
+        tmp[i] = '\0';                  /* tmp now contains the target */
+
+        char* val = VLlookup(tmp);      /* Look up tmp and return mapped val */
+        if (strcmp(val, "") == 0)
+            break;
+
+        for(cp = str; *cp; cp++) {
+            if (*cp != '$') {
+                fs_addch(&fname, *cp);
+            } else {
+                fs_addstr(&fname, val);
+                int j = 0;
+                for (j = 0; j < i; j++)
+                    cp++;
+            
+            }
+        }
+        fs_addch(&fname, '\0');
+        str = fs_getstr(&fname);
+    }
+    return fs_getstr(&fname);
+}   
+
 
 int main(int argc, char** argv)
 {
@@ -35,11 +85,20 @@ int main(int argc, char** argv)
         fp = stdin;
         prompt = DFL_PROMPT ;
     }
-        
+    
+            
 	setup();
+	while ((cmdline = next_cmd(prompt, fp)) != NULL) {
+        
+        char* newcmd = is_in_str(cmdline);
 
-	while ( (cmdline = next_cmd(prompt, fp)) != NULL ){
-		if ( (arglist = splitline(cmdline)) != NULL  ){
+        if (strcmp(newcmd,"") == 0) {
+            arglist = splitline(cmdline);
+        } else {
+            arglist = splitline(newcmd);
+        }
+        
+		if (arglist != NULL) {
 			result = process(arglist);
 			freelist(arglist);
 		}
